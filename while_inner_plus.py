94b88e7c6d8b8577d36b291528cb9f53762462ef
Katie Kartchner <katiemkartchner@gmail.com>
2025-10-06 20:47:58 -0600

    while_inner_plus.py

--- while_inner_plus.py
+++ while_inner_plus.py
@@ -1,28 +1,43 @@
 # %%
 """
-Write a python program that, given an input list, will filter the input above a user defined threshold. 
-This is to be done with a standard function.
-That is, given a list [1,2,3,4,5,6,7,8,9], and an argument (6), it should return [1,2,3,4,5,6]
-"""
+Write a python program that, given an input list of any level of complexity/nestedness, will return the inner most list plus 1. 
+This is to be done with a while loop. Note: the input will contain only integers or lists. 
+As an example:
+
+input_list = [1,2,3,4,[5,6,7,[8,9]]]
+
+your_py_program.py input_list
+
+will produce:
+
+[9,10]
+
+That is [8, 9] (the inner most list) plus 1 -> [9, 10]
+""" 
 
 # %%
-#insert a list and a threshold
-input_list=[1,2,3,4,5,6,7,8,9]
-threshold=6
-print("list:",input_list)
-print("threshold:",threshold)
+#change value of list to any list
+input_list = [1,2,3,4,[5,6,7,[8,9]]]
+print(input_list)
 
 # %%
-#make a standard function will filter the input above a user defined threshold
-def filter_function(input_list, threshold):
-    filter_list=[]
-    for x in input_list: 
-        if x <= threshold:
-            filter_list.append(x)
-    return(filter_list)
-    
+#while loop function called find_inner will find the inner most list within the list
+def find_inner(input_list): 
+    current_list=input_list
+    #here we need to identify the first nested list
+    while any(isinstance(x, list) for x in current_list):
+        for x in current_list:
+            if isinstance(x, list):
+                current_list= x
+                #current_list is now the inner most list so we break
+                break
+        #need to add 1 to the inner most list
+    return [x + 1 for x in current_list]
 
 # %%
-print(filter_function(input_list,threshold))
+print(find_inner(input_list)) 
+
+
+
 
 
